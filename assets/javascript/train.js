e43d7bb9bf7ae0ebe522e70b82d60fab20dfa1f6
Scottie <avscott@pacbell.net>
2019-09-06 23:16:45 -0700

    update firebase links

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -1,12 +1,12 @@
 // Your web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyA8jNNqsLWzqEld1ZTzy9Ad4YkufqKHr8A",
-    authDomain: "countdown-c41f0.firebaseapp.com",
-    databaseURL: "https://countdown-c41f0.firebaseio.com",
-    projectId: "countdown-c41f0",
-    storageBucket: "countdown-c41f0.appspot.com",
-    messagingSenderId: "836309319452",
-    appId: "1:836309319452:web:8a391c915e28616a"
+    apiKey: "AIzaSyALhmV6s1bBlGbk1IxXHMjE1PvQLtPPcUo",
+    authDomain: "train-time-956f3.firebaseapp.com",
+    databaseURL: "https://train-time-956f3.firebaseio.com",
+    projectId: "train-time-956f3",
+    storageBucket: "gs://train-time-956f3.appspot.com/",
+    messagingSenderId: "962130239195",
+    appId: "1:962130239195:web:789659593d24973fdc36fe"
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
