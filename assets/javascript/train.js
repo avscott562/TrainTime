ccdf636e622ebc327727f3e95bb032432f88fa25
Scottie <avscott@pacbell.net>
2019-09-08 22:33:30 -0700

    add click function to submit button

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -13,3 +13,30 @@ var firebaseConfig = {
 
   // Create a variable to reference the database
   var database = firebase.database();
+
+  $('#submit').on("click", function(event) {
+    // prevent form from submitting
+    event.preventDefault();
+    //create a new row for the table
+    var newRow = $('<tr>');
+    //get submitted train name and add to row
+    var newTrain = $('<td>');
+    newTrain.text($('#train-name').val().trim());
+    //get submitted destination and add to row
+    var newDest = $('<td>');
+    newDest.text($('#destination').val().trim());
+    //get submitted train time and add to row
+    // var newTime = $('<td>');
+    var time = $('#time').val().trim()
+    // newTime.text(time);
+    // console.log(time);
+    // var convertedTime = moment(date, "yyyy-mm-dd");
+    var newFreq = $('<td>');
+    newFreq.text($('#frequency').val().trim());
+    var newArrive = $('<td>');
+    newArrive.text('30');
+    var newMinutes = $('<td>');
+    newMinutes.text(time);
+    newRow.append(newTrain, newDest, newFreq, newArrive, newMinutes);
+    $('#schedule').append(newRow);
+});
